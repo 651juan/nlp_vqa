d2770f326f2da63dc552f766ca33728b0183f1a1
dinikolop <dimnik90@gmail.com>
2017-12-04 19:50:13 +0100

    First version of lstm: not complete

--- parameters.py
+++ parameters.py
@@ -2,13 +2,21 @@ image_features_path = './download/VQA_image_features.h5'
 img_features2id_path = './download/VQA_img_features2id.json'
 imgid2imginfo_path = './data/imgid2imginfo.json'
 
-
+# General parameters
 batch_size = 128
-embedding_dim = 64
-img_features_dim = 2048
-
-hidden_dims = []
-transformations = []
-
 lr = 0.001
 epochs = 100
+
+# Embedding parameters
+embedding_dim = 64
+
+# LSTM parameters
+lstm_dim = 128
+
+# Image feature parameters
+img_features_dim = 2048
+
+# Classifier parameters
+hidden_dims = [100]
+transformations = ["relu"]
+
